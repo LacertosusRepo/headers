a960b98676a4b2ca8d2bde1894bbe6f5784ee28d
LacertosusDeus <LacertosusThemes@gmail.com>
2022-02-17 13:06:45 -0700

    Update CNVisualIdentityContactAvatarProvider.h

--- ContactsUI/CNVisualIdentityContactAvatarProvider.h
+++ ContactsUI/CNVisualIdentityContactAvatarProvider.h
@@ -4,6 +4,6 @@ API_AVAILABLE(ios(14))
 @interface CNVisualIdentityContactAvatarProvider : NSObject
 
 - (instancetype)initWithSettings:(CNAvatarImageRendererSettings *)arg1;
-- (CNCancelationToken *)avatarImageForContact:(CNContact *)arg1 withSize:(CGSize)arg2 includePlaceholder:(BOOL)arg3 imageHandler:()arg4;
+- (CNCancelationToken *)avatarImageForContact:(CNContact *)arg1 withSize:(CGSize)arg2 includePlaceholder:(BOOL)arg3 imageHandler:(void (^)(UIImage *image))arg4;
 
 @end
