9e02305e9cabfefd99f9a50b784c8c4fd489e7cb
LacertosusDeus <LacertosusThemes@gmail.com>
2022-02-17 12:52:04 -0700

    Update CNAvatarImageRendererSettings.h

--- ContactsUI/CNAvatarImageRendererSettings.h
+++ ContactsUI/CNAvatarImageRendererSettings.h
@@ -1,9 +1,16 @@
-@class CNContactStore, PNPersonaStore;
+@class CNContactStore, PNPersonaStore, CNSchedulerProvider;
 
+API_AVAILABLE(ios(14))
 @interface CNAvatarImageRendererSettings : NSObject
 
 + (instancetype)defaultSettings;
++ (instancetype)defaultSettingsWithCacheSize:(NSUInteger)arg1;
 + (instancetype)settingsWithContactStore:(CNContactStore *)arg1;
++ (instancetype)settingsWithContactStore:(CNContactStore *)arg1 cacheSize:(NSUInteger)arg2;
++ (instancetype)settingsWithContactStore:(CNContactStore *)arg1 cacheSize:(NSUInteger)arg2 schedulerProvider:(CNSchedulerProvider *)arg3;
++ (instancetype)settingsWithContactStore:(CNContactStore *)arg1 cacheSize:(NSUInteger)arg2 schedulerProvider:(CNSchedulerProvider *)arg3 concurrentCaches:(BOOL)arg4;
++ (instancetype)settingsWithContactStore:(CNContactStore *)arg1 schedulerProvider:(CNSchedulerProvider *)arg3;
 + (instancetype)settingsWithContactStore:(CNContactStore *)arg1 personaStrore:(PNPersonaStore *)arg2;
++ (instancetype)offMainThreadSynchronousRenderingOnlySettingsWithCacheSize:(NSUInteger)arg1;
 
 @end
