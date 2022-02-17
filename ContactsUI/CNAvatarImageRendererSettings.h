0cf96f5a966b6f412379b55f7d24fdeb24b925bb
LacertosusDeus <LacertosusThemes@gmail.com>
2022-02-17 15:29:40 -0700

    Update CNAvatarImageRendererSettings.h

--- ContactsUI/CNAvatarImageRendererSettings.h
+++ ContactsUI/CNAvatarImageRendererSettings.h
@@ -4,13 +4,15 @@ API_AVAILABLE(ios(14))
 @interface CNAvatarImageRendererSettings : NSObject
 
 + (instancetype)defaultSettings;
-+ (instancetype)defaultSettingsWithCacheSize:(NSUInteger)arg1;
-+ (instancetype)settingsWithContactStore:(CNContactStore *)arg1;
-+ (instancetype)settingsWithContactStore:(CNContactStore *)arg1 cacheSize:(NSUInteger)arg2;
-+ (instancetype)settingsWithContactStore:(CNContactStore *)arg1 cacheSize:(NSUInteger)arg2 schedulerProvider:(CNSchedulerProvider *)arg3;
-+ (instancetype)settingsWithContactStore:(CNContactStore *)arg1 cacheSize:(NSUInteger)arg2 schedulerProvider:(CNSchedulerProvider *)arg3 concurrentCaches:(BOOL)arg4;
-+ (instancetype)settingsWithContactStore:(CNContactStore *)arg1 schedulerProvider:(CNSchedulerProvider *)arg3;
-+ (instancetype)settingsWithContactStore:(CNContactStore *)arg1 personaStrore:(PNPersonaStore *)arg2;
-+ (instancetype)offMainThreadSynchronousRenderingOnlySettingsWithCacheSize:(NSUInteger)arg1;
++ (instancetype)defaultSettingsWithCacheSize:(NSUInteger)cacheSize;
+
++ (instancetype)settingsWithContactStore:(CNContactStore *)contactStore;
++ (instancetype)settingsWithContactStore:(CNContactStore *)contactStore cacheSize:(NSUInteger)cacheSize;
++ (instancetype)settingsWithContactStore:(CNContactStore *)contactStore cacheSize:(NSUInteger)cacheSize schedulerProvider:(CNSchedulerProvider *)schedulerProvider;
++ (instancetype)settingsWithContactStore:(CNContactStore *)contactStore cacheSize:(NSUInteger)cacheSize schedulerProvider:(CNSchedulerProvider *)schedulerProvider concurrentCaches:(BOOL)concurrentCaches;
++ (instancetype)settingsWithContactStore:(CNContactStore *)contactStore schedulerProvider:(CNSchedulerProvider *)schedulerProvider;
++ (instancetype)settingsWithContactStore:(CNContactStore *)contactStore personaStrore:(PNPersonaStore *)personaStore;
+
++ (instancetype)offMainThreadSynchronousRenderingOnlySettingsWithCacheSize:(NSUInteger)cacheSize;
 
 @end
